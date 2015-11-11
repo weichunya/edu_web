a4154750854e6f2c57c066bca124e715c8160c3c
GaoDaWei <649187156@qq.com>
2015-11-11 17:09:13 +0800

    hehe

--- app/views/index/index.blade.php
+++ app/views/index/index.blade.php
@@ -29,19 +29,19 @@
             </div>
         </div>
     </div>
-    <div class="dbh-partner">
+    <div class="dbh-partner" style="display:none;">
         <!--TODO: my partner-->
     </div>
     <div class="dbh-footer">
         <div class="dbh-info">
-            <div class="dbh-partner-link">
+            <div class="dbh-partner-link" style="display:none;">
                 <span>友情链接</span> :&nbsp;&nbsp;苹果助手 | iOS8.4越狱 | 苹果手机助手 | 手机助手 |
             </div>
             <div class="dbh-line"></div>
             <div class="dbh-copyright">
-                <p>教程与帮助 | 安卓开发者平台 | 意见反馈 | 投诉举报 | 诚聘英才 | 友情链接 | 联系我们</p>
-                <p>PP助手是一款最受欢迎的手机助手。同时支持苹果及安卓设备的软件、游戏、壁纸铃声资源的下载安装及管理工具，致力于做一款注重用户体验的安卓苹果手机助手设备管理软件</p>
-                <p>Copyright @ 2009-2015 Aihe Internet Technology Co.,Ltd.All rights reserved.广州爱禾网络技术有限公司 版权所有 粤ICP备14003765号-2</p>
+                <p style="display:none;">教程与帮助 | 安卓开发者平台 | 意见反馈 | 投诉举报 | 诚聘英才 | 友情链接 | 联系我们</p>
+                <p>夺宝会，仅需1元钱，夺得好商品</p>
+                <p>北京小树网络技术有限公司 版权所有 京ICP备13034049号-1</p>
             </div>
         </div>
     </div>
